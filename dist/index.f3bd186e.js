bd263ef6b0ffdb7653a048a05a9fb4f6af0dfbdb
j-syun <jesschzn@gmail.com>
2022-06-05 16:16:50 +1000

    added stopwatch

--- dist/index.f3bd186e.js
+++ dist/index.f3bd186e.js
@@ -117,8 +117,9 @@ close_btns.forEach((btn)=>{
     btn.addEventListener("click", ()=>{
         btn.parentElement.style.display = "none";
     });
-}); //==================drop down================================
- /*var dropdown = document.getElementsByClassName("dropdown-btn");
+});
+//==================drop down================================
+/*var dropdown = document.getElementsByClassName("dropdown-btn");
 var i;
 
 for (i = 0; i < dropdown.length; i++) {
@@ -131,7 +132,7 @@ for (i = 0; i < dropdown.length; i++) {
       dropdownContent.style.display = "block";
     }
   });
-}*/  /* let arrow = document.querySelectorAll(".arrow");
+}*/ /* let arrow = document.querySelectorAll(".arrow");
   for (var i = 0; i < arrow.length; i++) {
     arrow[i].addEventListener("click", (e)=>{
    let arrowParent = e.target.parentElement.parentElement;//selecting main parent of arrow
@@ -143,6 +144,41 @@ for (i = 0; i < dropdown.length; i++) {
   console.log(sidebarBtn);
   sidebarBtn.addEventListener("click", ()=>{
     sidebar.classList.toggle("close");
-  });*/ 
+  });*/ /*---------========= STOPWATCH JS ======-----------------------*/ /* -------===== CODE REFERENCE: https://github.com/TylerPottsDev/yt-js-stopwatch ======----------- */ // Global variables
+const time_el = document.querySelector('.watch .time');
+const start_btn = document.getElementById('start');
+const stop_btn = document.getElementById("stop");
+const reset_btn = document.getElementById("reset");
+let seconds = 0;
+let interval = null;
+// Event listeners
+start_btn.addEventListener('click', start);
+stop_btn.addEventListener("click", stop);
+reset_btn.addEventListener("click", reset);
+// Update the timer
+function timer() {
+    seconds++;
+    // Format our time
+    let hrs = Math.floor(seconds / 3600);
+    let mins = Math.floor((seconds - hrs * 3600) / 60);
+    let secs = seconds % 60;
+    if (secs < 10) secs = '0' + secs;
+    if (mins < 10) mins = "0" + mins;
+    if (hrs < 10) hrs = "0" + hrs;
+    time_el.innerText = `${hrs}:${mins}:${secs}`;
+}
+function start() {
+    if (interval) return;
+    interval = setInterval(timer, 1000);
+}
+function stop() {
+    clearInterval(interval);
+    interval = null;
+}
+function reset() {
+    stop();
+    seconds = 0;
+    time_el.innerText = '00:00:00';
+}
 
 //# sourceMappingURL=index.f3bd186e.js.map
--- public/script.js
+++ public/script.js
@@ -196,3 +196,55 @@ for (i = 0; i < dropdown.length; i++) {
   sidebarBtn.addEventListener("click", ()=>{
     sidebar.classList.toggle("close");
   });*/
+
+
+  /*---------========= STOPWATCH JS ======-----------------------*/
+  /* -------===== CODE REFERENCE: https://github.com/TylerPottsDev/yt-js-stopwatch ======----------- */
+  // Global variables
+const time_el = document.querySelector('.watch .time');
+const start_btn = document.getElementById('start');
+const stop_btn = document.getElementById("stop");
+const reset_btn = document.getElementById("reset");
+
+let seconds = 0;
+let interval = null;
+
+// Event listeners
+start_btn.addEventListener('click', start);
+stop_btn.addEventListener("click", stop);
+reset_btn.addEventListener("click", reset);
+
+// Update the timer
+function timer () {
+	seconds++;
+
+	// Format our time
+	let hrs = Math.floor(seconds / 3600);
+	let mins = Math.floor((seconds - (hrs * 3600)) / 60);
+	let secs = seconds % 60;
+
+	if (secs < 10) secs = '0' + secs;
+	if (mins < 10) mins = "0" + mins;
+	if (hrs < 10) hrs = "0" + hrs;
+
+	time_el.innerText = `${hrs}:${mins}:${secs}`;
+}
+
+function start () {
+	if (interval) {
+		return
+	}
+
+	interval = setInterval(timer, 1000);
+}
+
+function stop () {
+	clearInterval(interval);
+	interval = null;
+}
+
+function reset () {
+	stop();
+	seconds = 0;
+	time_el.innerText = '00:00:00';
+}
